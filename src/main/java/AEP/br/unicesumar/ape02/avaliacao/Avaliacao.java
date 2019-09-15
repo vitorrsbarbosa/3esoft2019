61160fb4914f4daa61d13c6baf18e40f33efc5e9
vitor <viitorrsbarbosa@gmail.com>
2019-09-14 21:43:26 -0300

    criacao de requisições para chamada de alunos, avaliações e o conceito

--- src/main/java/AEP/br/unicesumar/ape02/avaliacao/Avaliacao.java
+++ src/main/java/AEP/br/unicesumar/ape02/avaliacao/Avaliacao.java
@@ -7,32 +7,53 @@ import javax.persistence.Id;
 
 public class Avaliacao {
 
+    @Id
+    private String id;
     
+    public String getId() {
+        return id;
+    }
+
+    public Avaliacao() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+
+
+
+
+
     @Entity
     public static class Nota {
 
         @Id
         private String id;
-        private Double valor;
-
+        private Float nota;
+        private Float maximo;
+        private Float minimo;
         public Nota() {
             this.id = UUID.randomUUID().toString();
-            
+            if(this.nota < minimo || this.nota > maximo){
+                throw new RuntimeException("nota inválida");
+        }else
+            this.nota = nota;
+
         }
+
         public String getId() {
             return id;
         }
 
-        public Double getValor() {
-            return valor;
+        public Float getNota() {
+            return nota;
         }
 
-        public void setValor(Double valor) {
-            this.valor = valor;
+        public Float getMaximo() {
+            return 10f;
         }
 
-        public void setId(String id) {
-            this.id = id;
+        public Float getMinimo() {
+            return 0f;
         }
     }
 }
